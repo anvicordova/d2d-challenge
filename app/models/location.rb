78b6dffe30540fe73fb87b933680f65967b0736a
Angela <anvicordova@gmail.com>
2022-02-28 23:08:52 -0500

    Add model annotations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id         :bigint           not null, primary key
+#  latitude   :decimal(, )
+#  longitud   :decimal(, )
+#  sent_at    :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  vehicle_id :integer
+#
 class Location < ApplicationRecord
   belongs_to :vehicle
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: vehicles
+#
+#  id         :bigint           not null, primary key
+#  uuid       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  deleted_at :datetime
+#
 class Vehicle < ApplicationRecord
   acts_as_paranoid
 
